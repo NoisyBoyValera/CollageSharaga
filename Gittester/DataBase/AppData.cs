08cc509c581f7fb113225d590f584fdbe6e3a877
NoisyBoyValera <sasasasasasa11321312312@mail.ru>
2020-11-11 01:37:41 +0300

    first part

---
+++ Gittester/DataBase/AppData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gittester.DataBase
+{
+    class AppData
+    {
+        public static DataBaseEntities Context
+        {
+            get; set;
+        } = new DataBaseEntities();
+    }
+}
---
+++ Gittester/Windows/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Gittester.DataBase;
+
+namespace Gittester
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrWhiteSpace(EmailTextBox.Text) && !String.IsNullOrWhiteSpace(PasswordTextBox.Text))
+                {
+                    Users CurrentUser = AppData.Context.Users.Where(c => c.Email == EmailTextBox.Text && c.Password == PasswordTextBox.Text).FirstOrDefault();
+                    if (CurrentUser != null)
+                    {
+                        MessageBox.Show("Вы успешно авторизовались!", "Внимание", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Введены неверные данные!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Не все поля заполнены", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
